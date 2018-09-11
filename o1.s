a0e6aa0152a7dfc5438fa596a2403373041eb4eb
Oskar Østby <31510521+Oskiboy@users.noreply.github.com>
2018-09-11 16:46:27 +0200

    Update o1.s
    Adds correct comment syntax and correct branching.

--- o1.s
+++ o1.s
@@ -10,10 +10,8 @@
 
 Start:
     bl      poll_button
-    push    {lr}
     bne     turn_on_led
-    beq     turn_off_led
-    pop     {lr}
+    bl     turn_off_led
     b Start
 
 
@@ -22,17 +20,17 @@ poll_button:
     ldr r1, =PORT_SIZE
     mul r0, r0, r1
     ldr r1, =GPIO_BASE
-    add r0, r0, r1          ;r0 now contains address of the button port
-    ldr r1, =GPIO_PORT_DIN  ;r1 contains the input bits offset
-    ldr r3, [r0, r1]        ;r3 now contains the input bits.
+    add r0, r0, r1          //r0 now contains address of the button port
+    ldr r1, =GPIO_PORT_DIN  //r1 contains the input bits offset
+    ldr r3, [r0, r1]        //r3 now contains the input bits.
     
     mov r2, #1
-    lsl r2, r2, #BUTTON_PIN ;r2 contains the byte to or with the inputs to see if the button is pressed.
+    lsl r2, r2, #BUTTON_PIN //r2 contains the byte to or with the inputs to see if the button is pressed.
 
-    and r0, r3, r2          ;compare the input with the bit we want
+    and r0, r3, r2          //compare the input with the bit we want
     cmp r0, #0
 
-    mov pc, lr              ;go back to call location
+    mov pc, lr              //go back to call location
 
 turn_off_led:
     ldr r0, =PORT_E
@@ -59,7 +57,8 @@ turn_on_led:
     lsl r2, r2, #LED_PIN
     ldr r1, =GPIO_PORT_DOUTSET
     str r2, [r0, r1]
-    mov pc, lr
+    
+    b Start
 
-NOP ; no touchey
+NOP // no touchey
 
